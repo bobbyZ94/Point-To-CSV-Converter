3d541821e791433d66c78a083fe23da9a5a00ac5
bobbyZ94 <r.zioltkowski@gmail.com>
2022-08-18 01:35:48 +0200

    clusterfuck, why does react dont want to re render when array is changed?

--- components/MapWrapper.js
+++ components/MapWrapper.js
@@ -1,50 +1,95 @@
 import React, { useEffect, useState } from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
-import Marker from './Marker'
+import { GoogleMap, LoadScript, useGoogleMap } from '@react-google-maps/api'
+import uniqid from 'uniqid'
 
 const center = {
   lat: 49.88283564019705,
   lng: 8.652589702130772,
 }
 
-function MyComponent() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GMAPS_API_KEY,
-  })
-
-  const [map, setMap] = React.useState(null)
-
-  const onLoad = React.useCallback((map) => {
-    // const bounds = new window.google.maps.LatLngBounds(center)
-    // map.fitBounds(bounds)
-    setMap(map)
-  }, [])
-
-  const onUnmount = React.useCallback((map) => {
-    setMap(null)
-  }, [])
-
+function MapWrapper() {
+  const map = useGoogleMap()
   // Add mousclick listener for coordinates
   const [markersCoordinates, setMarkersCoordinates] = useState([])
-  console.log(markersCoordinates[0]?.toString())
+  console.log(markersCoordinates)
   useEffect(() => {
     if (map) {
       window.google.maps.event.addListener(map, 'click', (event) => {
-        setMarkersCoordinates([...markersCoordinates, event.latLng])
+        setMarkersCoordinates([...markersCoordinates, { latLng: event.latLng, id: uniqid(), name: '' }])
       })
     }
   })
-
-  return isLoaded ? (
-    <GoogleMap mapContainerClassName="w-full h-full" center={center} zoom={10} onLoad={onLoad} onUnmount={onUnmount}>
-      {markersCoordinates.map((coordinates, index) => (
-        <Marker position={coordinates} map={map} label={String(index + 1)} />
-      ))}
-    </GoogleMap>
-  ) : (
-    <div>Loading ...</div>
+  function Marker({ position, map, id }) {
+    const [marker, setMarker] = useState()
+    const [infoWindow, setInfoWindow] = useState()
+    useEffect(() => {
+      setMarker(new window.google.maps.Marker())
+      setInfoWindow(new window.google.maps.InfoWindow())
+    }, [])
+    if (marker) {
+      marker.setMap(map)
+      marker.setPosition(position)
+      marker.setLabel('A')
+      marker.setDraggable(true)
+    }
+    if (marker && infoWindow) {
+      infoWindow.setContent(`<div class="info-window-header"><strong>Punkt Nummer: ${id} Id: ${id}</strong></div>
+      <div class="info-window-text"><div>Latitude/Breite: ${
+        position &&
+        position
+          .toString()
+          .replace(/[\])}[{(]/g, '')
+          .split(/[, ]+ /)[0]
+      }</div>
+      <div>Longitude/Länge: ${
+        position &&
+        position
+          .toString()
+          .replace(/[\])}[{(]/g, '')
+          .split(/[, ]+ /)[1]
+      }</div></div>
+      <button class="info-window-button" id='${id}'>Lösche Punkt</button>`)
+    }
+    useEffect(() => {
+      marker?.addListener('click', () => {
+        infoWindow.open({
+          anchor: marker,
+          map,
+          shouldFocus: false,
+        })
+      })
+    })
+    useEffect(() => {
+      if (marker && infoWindow) {
+        window.google.maps.event.addListener(infoWindow, 'domready', () => {
+          const deleteButton = document.getElementById(id)
+          if (deleteButton) {
+            window.google.maps.event.addDomListener(deleteButton, 'click', () => {
+              setMarkersCoordinates([])
+              // marker.setMap(null)
+            })
+          }
+        })
+      }
+    })
+    // useEffect(() => {
+    //   if (marker) {
+    //     marker.addListener('dragend', (e) => {
+    //       setMarkersCoordinates([...markersCoordinates, (markersCoordinates[index] = marker.getPosition())]) // this === marker
+    //     })
+    //   }
+    // })
+    return null
+  }
+  return (
+    <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GMAPS_API_KEY}>
+      <GoogleMap mapContainerClassName="w-full h-full" center={center} zoom={10} onLoad={onLoad} onUnmount={onUnmount}>
+        {markersCoordinates?.map((coordinates) => (
+          <Marker position={coordinates.latLng} map={map} id={coordinates.id} />
+        ))}
+      </GoogleMap>
+    </LoadScript>
   )
 }
 
-export default React.memo(MyComponent)
+export default React.memo(MapWrapper)
