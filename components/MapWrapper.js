7f56d9cdb73951062fb4032fce8c002f21a18e67
bobbyZ94 <r.zioltkowski@gmail.com>
2022-08-17 18:51:13 +0200

    added infowindow to markers

--- components/MapWrapper.js
+++ components/MapWrapper.js
@@ -1,18 +1,50 @@
-import { Wrapper } from '@googlemaps/react-wrapper'
-import { useState } from 'react'
-import Map from './Map'
+import React, { useEffect, useState } from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
 import Marker from './Marker'
 
-export default function MapWrapper() {
-  const center = { lat: 49.88085964000008, lng: 8.649815043550795 }
-  const zoom = 8
-  const [markersCoordinates, setMarkersCoordinates] = useState()
-  console.log(markersCoordinates)
-  return (
-    <Wrapper apiKey={process.env.NEXT_PUBLIC_GMAPS_API_KEY}>
-      <Map center={center} zoom={zoom} setMarkersCoordinates={setMarkersCoordinates}>
-        <Marker position={markersCoordinates} />
-      </Map>
-    </Wrapper>
+const center = {
+  lat: 49.88283564019705,
+  lng: 8.652589702130772,
+}
+
+function MyComponent() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GMAPS_API_KEY,
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback((map) => {
+    // const bounds = new window.google.maps.LatLngBounds(center)
+    // map.fitBounds(bounds)
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback((map) => {
+    setMap(null)
+  }, [])
+
+  // Add mousclick listener for coordinates
+  const [markersCoordinates, setMarkersCoordinates] = useState([])
+  console.log(markersCoordinates[0]?.toString())
+  useEffect(() => {
+    if (map) {
+      window.google.maps.event.addListener(map, 'click', (event) => {
+        setMarkersCoordinates([...markersCoordinates, event.latLng])
+      })
+    }
+  })
+
+  return isLoaded ? (
+    <GoogleMap mapContainerClassName="w-full h-full" center={center} zoom={10} onLoad={onLoad} onUnmount={onUnmount}>
+      {markersCoordinates.map((coordinates, index) => (
+        <Marker position={coordinates} map={map} label={String(index + 1)} />
+      ))}
+    </GoogleMap>
+  ) : (
+    <div>Loading ...</div>
   )
 }
+
+export default React.memo(MyComponent)
